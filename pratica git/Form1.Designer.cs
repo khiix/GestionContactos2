2a9cbfce947020e59e3a498e56f9d25be4d33539
khiix <cristiandarien100@gmail.com>
2025-06-04 20:58:19 +0200

    Añadiendo mejoras en la gestión de contactos

---
+++ pratica git/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace pratica_git
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAyadir = new System.Windows.Forms.Button();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(137, 75);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(132, 22);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(137, 142);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(132, 22);
+            this.txtTelefono.TabIndex = 1;
+            this.txtTelefono.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(40, 79);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(53, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "telefono";
+            // 
+            // btnAyadir
+            // 
+            this.btnAyadir.Location = new System.Drawing.Point(72, 258);
+            this.btnAyadir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAyadir.Name = "btnAyadir";
+            this.btnAyadir.Size = new System.Drawing.Size(100, 28);
+            this.btnAyadir.TabIndex = 4;
+            this.btnAyadir.Text = "ayadir";
+            this.btnAyadir.UseVisualStyleBackColor = true;
+            this.btnAyadir.Click += new System.EventHandler(this.btnAyadir_Click);
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Location = new System.Drawing.Point(220, 258);
+            this.btEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(100, 28);
+            this.btEliminar.TabIndex = 5;
+            this.btEliminar.Text = "eliminar";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            this.btEliminar.Click += new System.EventHandler(this.btEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(72, 317);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(100, 28);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(220, 317);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(100, 28);
+            this.btnMostrar.TabIndex = 7;
+            this.btnMostrar.Text = "mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.btnAyadir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "<";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAyadir;
+        private System.Windows.Forms.Button btEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ pratica git/Form1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pratica_git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private const int MAX_CONTACTOS = 10;
+        private string[] nombres = new string[MAX_CONTACTOS];
+        private string[] telefonos = new string[MAX_CONTACTOS];
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnAyadir_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string telefono = txtTelefono.Text;
+            bool isValid = true;
+            for (int i = 0; i < MAX_CONTACTOS && isValid; i++)
+            {
+                if (nombres[i] == null)
+                {
+                    nombres[i] = nombre;
+                    telefonos[i] = telefono;
+                    MessageBox.Show("Contacto añadido.");
+                    isValid = false;
+                }
+                else if (nombres[i] == nombre && telefono == telefonos[i])
+                {
+                    MessageBox.Show("Error: Contacto ya registrado.");
+                    isValid = false;
+                }
+                else if (i == MAX_CONTACTOS - 1)
+                {
+                    MessageBox.Show("Error: Lista de contactos llena.");
+                }
+            }
+            txtNombre.Clear();
+            txtTelefono.Clear();
+        }
+
+        private void btEliminar_Click(object sender, EventArgs e)
+        {
+            string nombre = Interaction.InputBox("Ingrese el nombre del contacto a eliminar:", "Eliminar Contacto", "");
+            bool encontrado = false;
+            if (nombre == "" || nombre == null)
+            {
+                MessageBox.Show("Error: Nombre no puede estar vacío.");
+            }
+            else
+            {
+                for (int i = 0; i < MAX_CONTACTOS && !encontrado; i++)
+                {
+                    if (nombres[i] == nombre)
+                    {
+                        nombres[i] = "";
+                        telefonos[i] = "";
+                        MessageBox.Show("Contacto eliminado.");
+                        encontrado = true;
+                    }
+                }
+                if (!encontrado)
+                {
+                    MessageBox.Show("Error: Contacto no encontrado.");
+                }
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            string nombre = Interaction.InputBox("Ingrese el nombre del contacto a modificar:", "Modificar Contacto", "");
+            bool encontrado = false;
+            string telefonoNuevo = Interaction.InputBox("Ingrese el nuevo teléfono:", "Modificar Teléfono", "");
+            if (nombre == "" || nombre == null)
+            {
+                MessageBox.Show("Error: Nombre no puede estar vacío.");
+            }
+            for (int i = 0; i < MAX_CONTACTOS && !encontrado; i++)
+            {
+                if (nombres[i] == nombre)
+                {
+                    telefonos[i] = telefonoNuevo;
+                    MessageBox.Show("Teléfono actualizado.");
+                    encontrado = true;
+                }
+            }
+            if (!encontrado)
+            {
+                MessageBox.Show("Error: Contacto no encontrado.");
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string listaContactos = "Lista de contactos:\n";
+            for (int i = 0; i < MAX_CONTACTOS; i++)
+            {
+                if (!string.IsNullOrEmpty(nombres[i]))
+                {
+                    listaContactos += $"Nombre: {nombres[i]}, Teléfono: {telefonos[i]}\n";
+                }
+            }
+            MessageBox.Show(listaContactos);
+        }
+    }
+}

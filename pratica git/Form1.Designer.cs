17619c9d9fd47b59652fc6d6c96b27c07101951f
khiix <153730207+khiix@users.noreply.github.com>
2025-06-04 20:02:40 +0200

    Primer commit - Estructura y funciones básicas

---
+++ pratica git/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace pratica_git
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAyadir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(103, 61);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(103, 115);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 1;
+            this.txtTelefono.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(30, 64);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(42, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "telefono";
+            // 
+            // btnAyadir
+            // 
+            this.btnAyadir.Location = new System.Drawing.Point(54, 210);
+            this.btnAyadir.Name = "btnAyadir";
+            this.btnAyadir.Size = new System.Drawing.Size(75, 23);
+            this.btnAyadir.TabIndex = 4;
+            this.btnAyadir.Text = "ayadir";
+            this.btnAyadir.UseVisualStyleBackColor = true;
+            this.btnAyadir.Click += new System.EventHandler(this.btnAyadir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAyadir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAyadir;
+    }
+}
+
---
+++ pratica git/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pratica_git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        const int MAX_CONTACTOS = 10;
+        string[] nombres = new string[MAX_CONTACTOS];
+        string[] telefonos = new string[MAX_CONTACTOS];
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAyadir_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string telefono = txtTelefono.Text;
+
+            for (int i = 0; i < MAX_CONTACTOS; i++)
+            {
+                if (nombres[i] == null)
+                {
+                    nombres[i] = nombre;
+                    telefonos[i] = telefono;
+                    MessageBox.Show("Contacto añadido.");
+                    return;
+                }
+                else if (nombres[i] == nombre)
+                {
+                    MessageBox.Show("Error: Contacto ya registrado.");
+                    return;
+                }
+            }
+            MessageBox.Show("Lista llena.");
+        }
+    }
+}
